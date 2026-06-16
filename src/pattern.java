e0768173f647a6a442d60f36b1b2c5c79246878a
Harshpatel-2510 <patelharsh251005@gmail.com>
2026-06-16 20:08:46 +0530

    java loops practice challenges

--- src/pattern.java
+++ src/pattern.java
@@ -344,7 +344,25 @@ public class pattern {
 //        Pattern-19
         int n = 4;
         for (int row = 1; row <= n; row++) {
-
+            for (int col = 1; col <= row-1; col++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= (n*2)-(row*2)+1 ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int row = 1; row <= n; row++) {
+            if (row == 1) {
+                continue;
+            }
+            for (int col = 1; col <= (n-row); col++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= (2*row)-1 ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
 
 
