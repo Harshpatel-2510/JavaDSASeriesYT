f7643475e5d9180844ae1a5495ef5bfda2c490b2
Harshpatel-2510 <patelharsh251005@gmail.com>
2026-06-23 11:51:08 +0530

    java Method's concepts

--- src/Method.java
+++ src/Method.java
@@ -74,18 +74,126 @@ public class Method {
 
 
 //      ==> Program-5(Call by Value) <==
-    static void solve(int num) {
-        System.out.println("Inside solve:- " + num);
-        num = num * 10;
-        System.out.println("Inside solve:- " + num);
+//    static void solve(int num) {
+//        System.out.println("Inside solve:- " + num);
+//        num = num * 10;
+//        System.out.println("Inside solve:- " + num);
+//    }
+//
+//    static void main() {
+//        int num = 5;
+//        System.out.println("Inside main:- " + num);
+
+    /// /        "num" ke value me num ki copy value jati he jise call-by-value kehte he
+    ///
+    /// @return
+//        solve(num);
+//        System.out.println("Inside main:- " + num);
+//    }
+
+
+//    HW====> "Practice Questions" <====
+//      Program-1==>    Create a Method "printWelcomeMessage()" that print a Greeting
+
+//    static void printWelcomeMessage(){
+//        System.out.println("Hello Family..! This is my fisrt practice Question for Methods");
+//    }
+//
+//    static void main() {
+//        printWelcomeMessage();
+//    }
+
+
+//      Program-2==>    Create a Method ADD(int A,int B) That returns the sum.
+//    static int Add(int A, int B) {
+//        return A + B;
+//    }
+//
+//    static void main() {
+//        int sum = Add(25, 12);
+//        System.out.println("Value of sum is " + sum);
+//    }
+
+
+//      Program-3==>    Create a Method isEven(int number) that return true if number is Even.
+//    static boolean isEven(int number){
+//        if (number%2 == 0) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    static void main() {
+//        boolean value = isEven(25);
+//        System.out.println("Your Entered number is Even:- "+value);
+//    }
+
+
+//      Program-4==>    Create a Method GetMaximum(int A, int B) that return the larger number.
+//    static int GetMaximum(int A, int B) {
+//        if (A > B) {
+//            return A;
+//        } else {
+//            return B;
+//        }
+//    }
+//
+//    static void main() {
+//        int num1 = 10;
+//        int num2 = 25;
+//        if (num1 == num2) {
+//            System.out.println("Your Entered both number is Same, so larger number is " + num1);
+//        } else {
+//            int value = GetMaximum(num1, num2);
+//            System.out.println("Between this two number, Larger number is " + value);
+//        }
+//    }
+
+
+//      Program-5==>    Create a Method CalculatePercentage(int Obtained, int total).
+//    static double CalculatePercentage(double Obtained, int total){
+//        if (Obtained == 0) {
+//            return 0.0;
+//        }else {
+//            return ((double) Obtained / total)*100;
+//        }
+//    }
+//
+//    static void main() {
+//        double given_num = 452;
+//        int total = 500;
+//        double result = CalculatePercentage(given_num,total);
+//        System.out.println("Your Percentage:- "+result);
+//    }
+
+
+    //      Program-6==>    Create an Overloaded method display() with one int parameter or one string parameter.
+//    static void display(int a){
+//        System.out.println("Your Entered number:- "+a);
+//    }
+//    static void display(String name){
+//        System.out.println("Your Entered name:- "+name);
+//    }
+//
+//    static void main() {
+//        display(25);
+//        display("Harsh");
+//    }
+
+
+//      Program-7==>    Write a Method UpdateValue(int x) and verify that original variable in Main() does not change.
+    static void UpdateValue(int x){
+        System.out.println("Before update value:- "+x);
+        x=x*2;
+        System.out.println("After update valye:- "+x);
     }
 
     static void main() {
-        int num = 5;
-        System.out.println("Inside main:- " + num);
-//        "num" ke value me num ki copy value jati he jise call-by-value kehte he
-        solve(num);
-        System.out.println("Inside main:- " + num);
+        int x = 10;
+        System.out.println("Main Value of X:- "+x);
+        UpdateValue(x);
+        System.out.println("After performing Method program:- "+x);
     }
+
 
 }
