8f976c3e674b5aeb5d8c1bd176cfd877eeca8f03
Harshpatel-2510 <patelharsh251005@gmail.com>
2026-07-01 19:48:18 +0530

    java New concepts

---
+++ src/String.java
@@ -0,0 +1,7 @@
+public class String {
+    static void main() {
+
+//        What is String.? ===> String is just sequence of characters.The multiple characters stored and creat a Word it's call a String.
+        System.out.println("Hello");
+    }
+}
